99d2373ca95d9b4ce569491b6407dd8d9d5e49bf
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-05-28 01:35:29 +0530

    changes in Users entity

--- src/main/java/com/codewithakshay/fooddelivery/entity/Users.java
+++ src/main/java/com/codewithakshay/fooddelivery/entity/Users.java
@@ -8,7 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
@@ -20,19 +23,20 @@ public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long userId;
-	@NotEmpty(message = "user name should not be empty")
+	@NotBlank(message = "Name is Mandatory")
 	private String name;
-	@NotEmpty(message = "user email should not be empty")
-	private String Email;
-	@NotEmpty(message = "user password should not be empty")
-	private String Password;
-	@NotEmpty(message = "user phone should not be empty")
-	private long Phone;
-	@NotEmpty(message = "user address should not be empty")
-	private String Address;
-	@Column(name = "registration_data")
-	private Timestamp RegistrationDate;
+	@NotBlank(message = "Email is Mandatory")
+	private String email;
+	@NotBlank(message = "Password is Mandatory")
+	private String password;
+	@NotNull(message = "Phone is Mandatory")
+	private long phone;
+	@NotBlank(message = "Address is Mandatory")
+	private String address;
 	@Column(name = "last_login")
-	private Timestamp LastLogin;
+	private Timestamp lastLogin;
+	@UpdateTimestamp
+	@Column(name = "registration_date")
+	private Timestamp registrationDate;
 
 }
