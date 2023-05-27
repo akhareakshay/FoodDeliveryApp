28d4eab0c29df33a7a680765c34b64b96f2e51fd
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-05-27 23:41:52 +0530

    user entity added

--- src/main/java/com/codewithakshay/fooddelivery/entity/Users.java
+++ src/main/java/com/codewithakshay/fooddelivery/entity/Users.java
@@ -8,19 +8,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class Users {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long userId;
-	
+	@NotEmpty(message = "user name should not be empty")
 	private String name;
+	@NotEmpty(message = "user email should not be empty")
 	private String Email;
+	@NotEmpty(message = "user password should not be empty")
 	private String Password;
+	@NotEmpty(message = "user phone should not be empty")
 	private long Phone;
+	@NotEmpty(message = "user address should not be empty")
 	private String Address;
 	@Column(name = "registration_data")
 	private Timestamp RegistrationDate;
