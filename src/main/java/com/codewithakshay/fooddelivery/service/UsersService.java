417d5503e61675551d752933c84d2fda455c9cb2
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-05-28 01:37:13 +0530

    UsersService and UsersServiceImpl created

---
+++ src/main/java/com/codewithakshay/fooddelivery/service/UsersService.java
@@ -0,0 +1,9 @@
+package com.codewithakshay.fooddelivery.service;
+
+import com.codewithakshay.fooddelivery.entity.Users;
+
+public interface UsersService {
+
+	public Users setUsersData(Users users);
+
+}
---
+++ src/main/java/com/codewithakshay/fooddelivery/service/impl/UsersServiceImpl.java
@@ -0,0 +1,14 @@
+package com.codewithakshay.fooddelivery.service.impl;
+
+import com.codewithakshay.fooddelivery.entity.Users;
+import com.codewithakshay.fooddelivery.service.UsersService;
+
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public Users setUsersData(Users users) {
+
+		return null;
+	}
+
+}
