1dfffd86a029536b16dceaf78f8dae1c3760d648
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-05-28 14:41:30 +0530

    RestaurantService interface created

---
+++ src/main/java/com/codewithakshay/fooddelivery/service/RestaurantService.java
@@ -0,0 +1,11 @@
+package com.codewithakshay.fooddelivery.service;
+
+import java.util.List;
+
+import com.codewithakshay.fooddelivery.entity.Restaurant;
+
+public interface RestaurantService {
+
+	public List<Restaurant> searchRestaurant(Restaurant restaurant);
+
+}
