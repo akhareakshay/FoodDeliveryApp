49e5b7c2762d95bb3dffbbab463e6758f271e91b
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-05-28 12:03:57 +0530

    custom finder method

--- src/main/java/com/codewithakshay/fooddelivery/repository/UsersRepository.java
+++ src/main/java/com/codewithakshay/fooddelivery/repository/UsersRepository.java
@@ -1,5 +1,8 @@
 package com.codewithakshay.fooddelivery.repository;
 
+import java.util.List;
+
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,28 @@ import com.codewithakshay.fooddelivery.entity.Users;
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
+	public List<Users> findByUserId(Long userId);
+
+	public List<Users> findByName(String name);
+
+	public List<Users> findByEmail(String email);
+
+	public List<Users> findByPassword(String password);
+
+	public List<Users> findByPhone(Long phone);
+
+	public List<Users> findByAddress(String address);
+
+	public List<Users> findByNameAndEmail(String name, String email);
+
+	public List<Users> findByEmailAndPassword(String email, String password);
+
+	public List<Users> findByNameAndPhone(String name, Long phone);
+
+	public List<Users> findByNameAndAddress(String name, String address);
+
+	public List<Users> findByNameAndEmailAndPhone(String name, String email, String phone);
+
+	public List<Users> findByNameAndEmailAndPasswordAndPhoneAndAddress(String name, String email, String password,
+			Long phone, String address);
 }
