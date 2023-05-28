63ffe09f1a89d96c41bf7324b3ca1147e3b3650f
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-05-28 12:04:42 +0530

    search service created

--- src/main/java/com/codewithakshay/fooddelivery/service/UsersService.java
+++ src/main/java/com/codewithakshay/fooddelivery/service/UsersService.java
@@ -1,9 +1,13 @@
 package com.codewithakshay.fooddelivery.service;
 
+import java.util.List;
+
 import com.codewithakshay.fooddelivery.entity.Users;
 
 public interface UsersService {
 
 	public Users setUsersData(Users users);
+
+	public List<Users> searchUsers(Users users);
 
 }
