e49c2f05d610d575e80ebf7679cc916fb139ada8
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-05-28 14:42:07 +0530

    RestuarantServiceImpl service class created

---
+++ src/main/java/com/codewithakshay/fooddelivery/service/impl/RestaurantServiceImpl.java
@@ -0,0 +1,19 @@
+package com.codewithakshay.fooddelivery.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.codewithakshay.fooddelivery.entity.Restaurant;
+import com.codewithakshay.fooddelivery.service.RestaurantService;
+
+@Service
+public class RestaurantServiceImpl implements RestaurantService {
+
+	@Override
+	public List<Restaurant> searchRestaurant(Restaurant restaurant) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
